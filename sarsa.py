d368c5900461521a7e20e74ca0cf9f1bf5e34dcc
devkmro <kim.rodgers.mirikwa@gmail.com>
2023-05-01 15:57:44 +0300

    update environment function

--- sarsa.py
+++ sarsa.py
@@ -39,11 +39,27 @@ def update_q_table(
     new_q_value = q_table[action, state] + alpha * (
         reward + (gamma * next_state_value) - q_table[action, state]
     )
-
     # Replace old Q-value
     q_table[action, state] = new_q_value
-
     return q_table
+
+
+def update_environment(env, agent_pos, action, cliff_pos, goal_pos, rewards_cache, steps_cache, episode, max_steps, current_epsilon, q_table):
+    # Move agent to next position
+    agent_pos = move_agent(agent_pos, action)
+    # Mark visited path
+    env = mark_path(agent_pos, env)
+    # Determine next state
+    next_state = get_state(agent_pos)
+    # Compute and store reward
+    reward = get_reward(next_state, cliff_pos, goal_pos)
+    rewards_cache[episode] += reward
+    # Check whether game is over
+    game_over = check_game_over(episode, next_state, cliff_pos, goal_pos, steps_cache[episode], max_steps)
+    # Select next action using ε-greedy policy
+    next_action = epsilon_greedy_action(next_state, q_table, current_epsilon)
+
+    return agent_pos, env, reward, next_action, next_state, game_over
 
 
 def sarsa(sim_input, sim_output) -> (np.array, list):
@@ -71,37 +87,16 @@ def sarsa(sim_input, sim_output) -> (np.array, list):
             if steps_cache[episode] == 0:
                 # Get state corresponding to agent position
                 state = get_state(agent_pos)
-
                 # Select action using ε-greedy policy
                 action = epsilon_greedy_action(state, q_table, current_epsilon)
-
-            # Move agent to next position
-            agent_pos = move_agent(agent_pos, action)
-
-            # Mark visited path
-            env = mark_path(agent_pos, env)
-
-            # Determine next state
-            next_state = get_state(agent_pos)
-
-            # Compute and store reward
-            reward = get_reward(next_state, cliff_pos, goal_pos)
-            rewards_cache[episode] += reward
-
-            # Check whether game is over
-            game_over = check_game_over(episode, next_state, cliff_pos, goal_pos, steps_cache[episode], max_steps)
-
-            # Select next action using ε-greedy policy
-            next_action = epsilon_greedy_action(next_state, q_table, current_epsilon)
-
+            agent_pos, env, reward, next_action, next_state, game_over = update_environment(
+                env, agent_pos, action, cliff_pos, goal_pos, rewards_cache, steps_cache, episode, max_steps, current_epsilon, q_table)
             # Determine Q-value next state (on-policy)
             next_state_value = q_table[next_action][next_state]
-
             # Update Q-table
             q_table = update_q_table(
                 q_table, state, action, reward, next_state_value, gamma, alpha
             )
-
             # Update state and action
             state = next_state
             action = next_action
